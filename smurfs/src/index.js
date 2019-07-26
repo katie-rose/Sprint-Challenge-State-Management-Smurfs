c48f33366f6dc1e2f14c88d40ceb8cbdf342d03c
Katie Rose <katiefelten@gmail.com>
2019-07-26 12:07:56 -0400

    :rocket: actions, reducers, and components added

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,6 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./components/App";
+import App from "./App";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import { smurfReducer } from "./reducers/index";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(smurfReducer, applyMiddleware(thunk, logger));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
